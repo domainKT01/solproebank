f42fccfb717e45ce7377f1dcabbfc3b94e388f77
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-01-26 20:27:37 -0500

    menu adminLTE

---
+++ app/View/Components/AppLayout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+use App\Http\Controllers\Admin\HomeController;
+
+class AppLayout extends Component
+{
+    /**
+     * Get the view / contents that represents the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
+    {
+        return ("Buscando pagina");
+    }
+}
