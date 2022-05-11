d5a604d1ef7a9680bf02ac56a2638d9c03ffb951
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-05-11 18:15:56 -0500

    final 2

--- resources/views/livewire/admin/compare.blade.php
+++ resources/views/livewire/admin/compare.blade.php
@@ -21,7 +21,7 @@
                 <div class="mb-6">
                     <input placeholder="Sobrepeso" name="sobrepeso" class="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text">
                 </div>
-                <button type="submit" style="color: white;" class="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" aria-label="Default select example">Ingresar</button>
+                <button type="submit" style="color: black;" class="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" aria-label="Default select example">Ingresar</button>
             </form>
         </div>
     </div>
